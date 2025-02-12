2bd3748c25c18cfeb50c4f46731c0a965aea9e09
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-11 19:32:55 -0800

    small screen menu toggle working! slow transition included

---
+++ script.js
@@ -0,0 +1,10 @@
+// for toggling the menu
+
+var navLinks = document.getElementById('navLinks');
+
+function showMenu() {
+    navLinks.style.right = '0';
+}
+function hideMenu() {
+    navLinks.style.right = '-200px';
+}
